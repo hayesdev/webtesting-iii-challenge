a14a0b99603fb1a4fc7036e91ae0073ded810dc5
Greg Hayes <gthelight@gmail.com>
2020-01-28 15:04:15 -0600

    initial commit w first test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,15 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Display from "./Display";
+
+test("displays locked if lock button is clicked, unlocked if unlock button clicked ", () => {
+  /// AAA
+
+  // Arrange
+  const { getByLabelText, getByText } = render(<Display />);
+
+  const lockButton = getByText("Locked");
+
+  fireEvent.click(lockButton);
+});
